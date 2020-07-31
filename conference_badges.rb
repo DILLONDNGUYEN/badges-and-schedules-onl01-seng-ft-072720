f0f359dae84bb84e09575b7fd9449ed7907f7be9
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-31 03:55:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 
 def badge_maker(speaker)
-  
-  
+ return "Hello, my name is #{speaker}."
 end
+
+speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
